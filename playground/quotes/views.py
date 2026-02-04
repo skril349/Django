1cda579679ab6fd207aadc80fdd9df22692dd9a8
skril349 <tvives.cabaleiro@gmail.com>
2026-02-04 16:24:26 +0100

    adding logics

--- playground/quotes/views.py
+++ playground/quotes/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 def index(request):
     return HttpResponse("Hello from the quotes app!")
 
 def week(request, day):
-    return HttpResponse(f"Happy {day.capitalize()}!")
+    quote_text = None
+    if day == "monday":
+        quote_text = "Monday quote"
+    elif day == "tuesday":
+        quote_text = "Tuesday quote"
+    elif day == "wednesday":
+        quote_text = "Wednesday quote"
+    elif day == "thursday":
+        quote_text = "Thursday quote"
+    elif day == "friday":
+        quote_text = "Friday quote"
+    elif day == "saturday":
+        quote_text = "Saturday quote"
+    elif day == "sunday":
+        quote_text = "Sunday quote"
+    else:
+        return HttpResponseNotFound("Invalid day!")
+    return HttpResponse(f"Happy {day.capitalize()}! {quote_text}")
