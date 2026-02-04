6a93ad737d892d3584e6e67967798ccf9ef5399e
skril349 <tvives.cabaleiro@gmail.com>
2026-02-04 16:21:29 +0100

    adding dynamic routes

--- playground/quotes/urls.py
+++ playground/quotes/urls.py
@@ -4,13 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('monday', views.monday, name='monday'),
-    path('tuesday', views.tuesday, name='tuesday'),
-    path('wednesday', views.wednesday, name='wednesday'),
-    path('thursday', views.thursday, name='thursday'),
-    path('friday', views.friday, name='friday'),
-    path('saturday', views.saturday, name='saturday'),
-    path('sunday', views.sunday, name='sunday'),
+    path("<day>", views.week, name="week"),
 
 
 ]
