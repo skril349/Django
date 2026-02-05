08dce273b035c2484c90400512420886afdcce0e
skril349 <tvives.cabaleiro@gmail.com>
2026-02-05 13:51:18 +0100

    adding for loop on landing

--- playground/landing/views.py
+++ playground/landing/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from datetime import date
 # Create your views here.
 
+stack = ["python", "django", "javascript","PHP"]
 def home(request):
     return render(request, "landing/landing.html", {
         "name": "Antoni",
         "age":27,
         "date": date.today(),
+        "stack": stack
     })
