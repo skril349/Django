4d34dae0b10f6dbe5f0a0475a8c3a916388df329
skril349 <tvives.cabaleiro@gmail.com>
2026-02-05 13:13:23 +0100

    adding filters to html variables

--- playground/landing/views.py
+++ playground/landing/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
 
 def home(request):
     return render(request, "landing/landing.html", {
-        "name": "Antoni"
+        "name": "Antoni",
+        "age":27,
+        "date": date.today(),
     })
