bcd04dc78723a2e852c9041f2ee452d4e39ee814
skril349 <tvives.cabaleiro@gmail.com>
2026-02-19 11:29:40 +0100

    adding static files on settings

--- devilearn/config/settings.py
+++ devilearn/config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+from playground.config.settings import STATICFILES_DIRS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
