623eafbe43598cdb05d952c132cccea424ea4f72
skril349 <tvives.cabaleiro@gmail.com>
2026-02-25 13:25:35 +0100

    manage.py makemigrations to create Book and author models

--- playground/minilibrary/models.py
+++ playground/minilibrary/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null = True, blank = True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    published_date = models.DateField(null = True, blank = True)
+    pages = models.IntegerField()
+    isbn = models.CharField(max_length=50, unique=True)
+    
