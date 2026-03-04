52c80cde2a7df133e8f5da2be4a9f375b10e5b0a
skril349 <tvives.cabaleiro@gmail.com>
2026-03-04 10:20:43 +0100

    genres ORM

--- playground/minilibrary/models.py
+++ playground/minilibrary/models.py
@@ -5,7 +5,15 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birth_date = models.DateField(null = True, blank = True)
+    
+    def __str__(self):
+        return self.name
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -13,4 +21,8 @@ class Book(models.Model):
     published_date = models.DateField(null = True, blank = True)
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50, unique=True)
+    genres = models.ManyToManyField(Genre, related_name='books')
+    
+    def __str__(self):
+        return self.title
     
