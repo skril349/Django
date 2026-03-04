320b37d23af54b809ebf740bf49cce04aa10f37f
skril349 <tvives.cabaleiro@gmail.com>
2026-03-04 16:26:32 +0100

    migration loans

--- playground/minilibrary/models.py
+++ playground/minilibrary/models.py
@@ -50,6 +50,7 @@ class Loan(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='loans')
     loan_date = models.DateField(auto_now_add=True)
     return_date = models.DateField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
     
     def __str__(self):
         return f"{self.user.username} borrowed {self.book.title} on {self.loan_date}"
