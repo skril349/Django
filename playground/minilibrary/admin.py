40b6c0152453fd734d6cf4fa691ca0c6eb2d8339
skril349 <tvives.cabaleiro@gmail.com>
2026-03-05 10:53:02 +0100

    adding new values on tables bookAdmin

--- playground/minilibrary/admin.py
+++ playground/minilibrary/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan, Recommendation 
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author','pages', 'published_date')
+    search_fields = ('title', 'author__name')
+
 admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookDetail)
 admin.site.register(Review)
 admin.site.register(Loan)
