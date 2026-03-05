43b9cb21886ed151b634ee2d2f64f2af797c9e1e
skril349 <tvives.cabaleiro@gmail.com>
2026-03-05 11:18:46 +0100

    inlines many to many with book reviews

--- playground/minilibrary/admin.py
+++ playground/minilibrary/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan, Recommendation 
 
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
     list_display = ('title', 'author','pages', 'published_date')
     search_fields = ('title', 'author__name')
     list_filter = ('published_date', 'genres', 'author')
