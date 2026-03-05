ba6bc3e952f5577413d85951d2c5479f1dd2d93c
skril349 <tvives.cabaleiro@gmail.com>
2026-03-05 10:47:12 +0100

    adding admin models

--- playground/minilibrary/admin.py
+++ playground/minilibrary/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookDetail, Review, Loan, Recommendation 
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookDetail)
+admin.site.register(Review)
+admin.site.register(Loan)
+admin.site.register(Recommendation)
